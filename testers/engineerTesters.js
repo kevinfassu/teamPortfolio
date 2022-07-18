072a7fa9e9a58b4da8c1f1113577edb0f28fe4d5
livelovebll <70596067+livelovebll@users.noreply.github.com>
2022-07-18 09:45:27 -0700

    e-test code written

---
+++ testers/engineerTesters.js
@@ -0,0 +1,19 @@
+const Engineer = require('../lib/Engineer');
+
+test("setting up github via constructor", () => {
+    const engineerValue = "nosremetnarg";
+    const employee = new Engineer("kevin", 1, "kevin@engineer.com", engineerValue);
+    expect(employee.github).toBe(engineerValue);
+})
+
+test("getRole function", () => {
+    const engineerValue = "Engineer";
+    const employee = new Engineer("kevin", 1, "kevin@engineer.com", engineerValue);
+    expect(employee.getRole()).toBe(engineerValue);
+})
+
+test("Get gitHub", () => {
+    const engineerValue = "nosremetnarg";
+    const employee = new Engineer("kevin", 1, "kevin@engineer.com", engineerValue);
+    expect(employee.getGitHub()).toBe(engineerValue);
+})
