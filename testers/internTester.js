e438e9d3cbce5b3a4807690adf873972fb90c156
livelovebll <70596067+livelovebll@users.noreply.github.com>
2022-07-19 07:42:21 -0700

    interntester done

---
+++ testers/internTester.js
@@ -0,0 +1,19 @@
+const Intern = require("../lib/Intern");
+
+test("setting school through argument", () => {
+    const testValue = "University of California Los Angeles";
+    const employee = new Intern("kevin", 1, "kevin@kevin.com", testValue);
+    expect(employee.school).toBe(testValue);
+})
+
+test("getRole function", () => {
+    const testValue = "Intern";
+    const employee = new Intern("kevin", 1, "kevin@kevin.com", testValue);
+    expect(employee.getRole()).toBe(testValue);
+})
+
+test("Get School", () => {
+    const testValue = "University of California Los Angeles";
+    const employee = new Intern("kevin", 1, "kevin@kevin.com", testValue);
+    expect(employee.getSchool()).toBe(testValue);
+});
